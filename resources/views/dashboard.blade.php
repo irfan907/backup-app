e4383ef8e897d0607338ef6efcd0038669ccf968
irfan907 <75222952+irfan907@users.noreply.github.com>
2022-11-29 16:37:50 +0500

    Added ui fixes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,5 +2,7 @@
 @section('title','Dashboard')
 
 @section('content')
-Dashboard Here
+<div class="bg-primary px-3 py-5 rounded text-center text-white">
+    <p class="fs-1"> <img style="height: 75px;" class="" src="{{ asset('volt/assets/img/brand/light.svg') }}" alt=""> Backup App</p>
+</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -187,7 +187,7 @@
             </div>
             <!-- Navbar links -->
             <ul class="navbar-nav align-items-center">
-                <li class="nav-item dropdown">
+                <li class="nav-item dropdown d-none">
                 <a class="nav-link text-dark notification-bell unread dropdown-toggle" data-unread-notifications="true" href="#" role="button" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
                     <svg class="icon icon-sm text-gray-900" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z"></path></svg>
                 </a>
@@ -315,14 +315,14 @@
                             @endforelse
                         </span>
                         </a>
-                    <a class="dropdown-item d-flex align-items-center" href="#">
+                    <!-- <a class="dropdown-item d-flex align-items-center" href="#">
                     <svg class="dropdown-icon text-gray-400 me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg>
                     My Profile
-                    </a>
-                    <a class="dropdown-item d-flex align-items-center" href="#">
+                    </a> -->
+                    <!-- <a class="dropdown-item d-flex align-items-center" href="#">
                     <svg class="dropdown-icon text-gray-400 me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"></path></svg>
                     Settings
-                    </a>
+                    </a> -->
                     <div role="separator" class="dropdown-divider my-1"></div>
                     <a class="dropdown-item d-flex align-items-center logout-button" type="submit" href="#">
                         <svg class="dropdown-icon text-danger me-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path></svg>                
@@ -388,14 +388,14 @@
         </div>
     </div>
 
-    <div class="card theme-settings bg-gray-800 theme-settings-expand" id="theme-settings-expand">
+    <!-- <div class="card theme-settings bg-gray-800 theme-settings-expand" id="theme-settings-expand">
         <div class="card-body bg-gray-800 text-white rounded-top p-3 py-2">
             <span class="fw-bold d-inline-flex align-items-center h6">
                 <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"></path></svg>
                 Settings
             </span>
         </div>
-    </div>
+    </div> -->
 
     <footer class="bg-white rounded shadow p-5 mb-4 mt-4 d-none">
         <div class="row">
